29ab44927bd95ea05757492bef71ce128850d455
Michelle Anderson <meobyte@gmail.com>
2015-12-01 20:14:22 -0800

    Add spock and lizard

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,23 @@
-CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(r p sc l sp)
+
+CHOICES = { 'r' => 'rock',
+            'p' => 'paper',
+            'sc' => 'scissors',
+            'l' => 'lizard',
+            'sp' => 'spock' }
+
+WINS = { 'sc' => %w(p, l),
+         'p' => %w(r sp),
+         'r' => %w(l sc),
+         'l' => %w(sp p),
+         'sp' => %w(sc r) }
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WINS[first].include?(second)
 end
 
 def display_result(player, computer)
@@ -23,18 +33,20 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{CHOICES.join(', ')}")
+    list_choices = ''
+    CHOICES.each { |k, v| list_choices += "#{v} (#{k})  " }
+    prompt("Choose one: #{list_choices}")
 
     choice = gets.chomp
-    if CHOICES.include?(choice)
+    if CHOICES.key?(choice)
       break
     else
       prompt("That's not a valid choice.")
     end
   end
 
-  computer_choice = CHOICES.sample
-  puts "You chose: #{choice}; Computer chose: #{computer_choice}"
+  computer_choice = CHOICES.keys.sample
+  puts "You chose: #{CHOICES[choice]}; Computer chose: #{CHOICES[computer_choice]}"
   display_result(choice, computer_choice)
 
   prompt("Do you want to play again?")
