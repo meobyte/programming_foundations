53ae9139a6dc858590afbbaa67ed4b8e2b2543f3
Michelle Anderson <meobyte@gmail.com>
2015-12-03 00:28:33 +0000

    Exercises: Easy 2

--- lesson_3/easy_2.rb
+++ lesson_3/easy_2.rb
@@ -1,66 +1,68 @@
-puts "Ex 1. ----------"
+puts 'Ex 1. ----------'
 
-ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 5843, 'Eddie' => 10,
+         'Marilyn' => 22, 'Spot' => 237 }
 
-puts ages.key?("Spot")
+puts ages.key?('Spot')
 
-puts "Ex 2. ----------"
+puts 'Ex 2. ----------'
 
-ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 5843, 'Eddie' => 10,
+         'Marilyn' => 22, 'Spot' => 237 }
 
 puts ages.values.inject(:+)
 
-puts "Ex 3. ----------"
+puts 'Ex 3. ----------'
 
-ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 402, 'Eddie' => 10 }
 
-puts ages.delete_if {|name, age| age >= 100 }
+puts ages.delete_if { |_, age| age >= 100 }
 
-puts "Ex 4. ----------"
+puts 'Ex 4. ----------'
 
-munsters_description = "The Munsters are creepy in a good way."
+munsters_description = 'The Munsters are creepy in a good way.'
 
-p munsters_description.capitalize
-p munsters_description.swapcase
-p munsters_description.downcase
-p munsters_description.upcase
+puts munsters_description.capitalize
+puts munsters_description.swapcase
+puts munsters_description.downcase
+puts munsters_description.upcase
 
-puts "Ex 5. ----------"
+puts 'Ex 5. ----------'
 
-ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 5843, 'Eddie' => 10 }
 
-additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+additional_ages = { 'Marilyn' => 22, 'Spot' => 237 }
 
-p ages.merge!(additional_ages)
+puts ages.merge!(additional_ages)
 
-puts "Ex 6. ----------"
+puts 'Ex 6. ----------'
 
-ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10,
-         "Marilyn" => 22, "Spot" => 237 }
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 5843, 'Eddie' => 10,
+         'Marilyn' => 22, 'Spot' => 237 }
 
-p ages.values.min
+puts ages.values.min
 
-puts "Ex 7. ----------"
+puts 'Ex 7. ----------'
 
-advice = "Few things in life are as important as house training your pet dinosaur."
+advice = 'Few things in life are as important as house training your pet dinosaur.'
 
-p advice.include?("Dino")
+puts advice.include?('Dino')
 
-puts "Ex 8. ----------"
-
-flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
-
-p flintstones.index { |name| name.include?("Be") }
-p flintstones.index { |name| name[0, 2] == "Be" }
-
-puts "Ex 9. ----------"
+puts 'Ex 8. ----------'
 
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 
-p flintstones.map! { |name| name[0, 3] }
+puts flintstones.index { |name| name.include?('Be') }
+puts flintstones.index { |name| name[0, 2] == 'Be' }
 
-puts "Ex 10. ----------"
+puts 'Ex 9. ----------'
 
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 
-p flintstones.map! { |name| name[0, 3] }
+puts flintstones.map! { |name| name[0, 3] }
+
+puts 'Ex 10. ----------'
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+puts flintstones.map! { |name| name[0, 3] }
